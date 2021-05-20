a346425f052f49dbe1615fa1a9b43680e2daf8db
Patrick Leckey <pleckey@users.noreply.github.com>
2021-05-20 09:56:16 -0400

    add more debug logging

--- importer-telus-health/__init__.py
+++ importer-telus-health/__init__.py
@@ -47,12 +47,14 @@ def create_location(uuid, name, address, postal_code, province):
         'province': province
     }
 
-    headers = {'Authorization': API_KEY, 'accept': 'application/json', 'Content-Type': 'application/json'}
+    headers = {'Authorization': API_KEY, 'Content-Type': 'application/json'}
     location_post = requests.post(request_path('locations/expanded/'), headers=headers, json=data)
+    logging.info(location_post.status_code)
     logging.info(location_post.request.url)
     logging.info(location_post.request.headers)
     logging.info(location_post.request.body)
     logging.info(location_post.text)
+    logging.info(location_post.content)
     location_id = location_post.json()['id']
     return location_id
 
